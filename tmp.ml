535fb0952df363b1824ca17a4862e054f08f7f2a
Gwendoline <chouasne@clipper.ens.fr>
2015-10-30 00:59:44 +0100

    ai fait les bencoded_to__Answer

--- tmp.ml
+++ tmp.ml
@@ -69,12 +69,86 @@ let bencodeQuery  =
 
 type bencoded = BDic of (string * bencoded) list | BString of string;;
 
+exception Bad_Answer of string;;
+
+
+(*bencode_to_idAndDic : bencoded -> (string*BDic 'a)*)
+let bencoded_to_idAndDic b = 
+  match b with
+  |BString _-> raise (Bad_Answer "La réponse n'est pas un BDic")
+  |BDic l -> 
+    begin
+      if (List.length l != 3) then raise (Bad_Answer "Reponse de longueur incorrecte");
+      try 
+	let y = List.assoc "y" l in
+	begin
+	  match y with
+	  |BString "r" -> 
+	    let t = List.assoc "t" l in 
+	    begin
+	      match t with
+	      |BString a -> 
+		begin
+		  let r = List.assoc "r" l in
+		  begin
+		    match r with
+		    |BString _ ->  raise (Bad_Answer "Contenu du champ r invalide")
+		    |BDic dic -> (a, dic)
+		  end
+		end
+	      |BDic _ -> raise (Bad_Answer "Contenu du champ t invalide")
+	    end;
+	  |BString _ -> raise (Bad_Answer "Contenu du champ y invalide")
+	  |BDic _ -> raise (Bad_Answer "Contenu du champ y invalide")
+	end;
+      with Not_found -> raise (Bad_Answer "Champ manquant dans la réponse dans la reponse");
+    end
+;;
+
 (* bencoded_to_asnwer : bencoded -> answer *)
-let bencoded_to_answer = ();;
-  (* GWEN COMPLETE ICI STP, LOVE LOVE *)
+let bencoded_to_PingAnswer b= 
+  let (a, dic) = bencoded_to_idAndDic b in
+  
+  begin
+    if (List.length dic != 1) 
+    then raise (Bad_Answer "Longueur du champ r invalide")
+    else 
+      try 
+	let id = (List.assoc "id" dic) in 
+	begin
+	  match id with
+	  |BString num -> {ap_t = a; ap_id = num}
+	  |BDic _ ->  raise (Bad_Answer "Contenu du champ id invalide")
+	end
+      with Not_found -> raise (Bad_Answer "Champ id manquant dans la réponse")
+  end
 
 
+let decoupe_nodes s = 
+  (String.sub s 0 20)::(String.sub s 20 20)::(String.sub s 40 20)::(String.sub s 60 20)::(String.sub s 80 20)::(String.sub s 100 20)::(String.sub s 120 20)::(String.sub s 140 20)::[]
+;;
       
       
-      
-      
+let bencoded_to_Find_NodesAnswer b= 
+  let (a, dic) = bencoded_to_idAndDic b in
+  if (List.length dic != 2) 
+  then raise (Bad_Answer "Longueur du champ r invalide")
+  else 
+    try 
+      let id = (List.assoc "id" dic) in 
+      begin
+	match id with
+	|BString num -> 
+	  begin 
+	    let nodes = (List.assoc "nodes" dic) in 
+	    begin
+	      match nodes with
+	      |BString id_nodes -> {afn_t = a; afn_id = num; afn_nodes = (decoupe_nodes id_nodes)}
+	      |BDic _ ->  raise (Bad_Answer "Contenu du champ nodes invalide")
+	    end
+	  end
+	|BDic _ ->  raise (Bad_Answer "Contenu du champ id invalide")
+      end
+    with Not_found -> raise (Bad_Answer "Champ id manquant dans la réponse")
+;;
+
