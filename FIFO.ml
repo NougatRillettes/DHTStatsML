ed209f3b32a247832740dcad1e3e20806c4db6f0
Arthur Carcano <arthur.carcano@orange.fr>
2015-11-22 00:22:26 +0100

    added Fifo

---
+++ FIFO.ml
@@ -0,0 +1,28 @@
+(* Limited size fifo *)
+
+type 'a fifo = (int ref * 'a list ref * int);;
+
+exception EmptyFifo;;
+
+let make m =
+  if m = 0 then failwith "Cannot create 0 sized fifo";
+  (ref 0, ref [], m)
+;;
+
+let pop (n,l,_) =
+  match (!n,!l) with
+    | (0,[]) -> raise EmptyFifo;
+    | (_,x::xs) -> decr n; l := xs; x
+    | _ -> failwith "Fifo STructure violated !"
+;;
+
+let empty (n,_,_) = 
+  !n == 0
+;;
+
+let push x (n,l,max) =
+  if !n == max then
+    l := List.rev (List.tl (List.rev !l));
+  incr n;
+  l := (x::!l)
+;;
---
+++ FIFO.mli
@@ -0,0 +1,11 @@
+(* Limited size fifo *)
+
+type 'a fifo = (int ref * 'a list ref * int);;
+
+val make : int -> 'a fifo;;
+
+val pop : 'a fifo -> 'a;;
+
+val empty : 'a fifo -> bool;;
+
+val push : 'a -> 'a fifo -> unit ;;
