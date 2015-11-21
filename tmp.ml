8baa64f0031609eb4765da9033ca642d101d9bbe
Arthur Carcano <arthur.carcano@orange.fr>
2015-11-21 19:43:37 +0100

    blblb

--- tmp.ml
+++ tmp.ml
@@ -11,6 +11,7 @@ type qFindNode =
       qfn_t : string;
       qfn_id : string;
       qfn_target : string;
+      qfn_want : int; (* 1(=01) : ip4, 2 (=10) ip6, 3 (=11) ip4 and ip6 *)
     };;
 
 type aPing =
@@ -56,9 +57,17 @@ let bencodeQFindNode r =
       bencodeStr r.qfn_id;
       "6:target";
       bencodeStr r.qfn_target;
+      "4:want";
+      begin
+      match r.qfn_want with
+        | 1 -> "l2:n4e"
+        | 2 -> "l2:n6e"
+        | 3 -> "l2:n42:n6e";
+      end;
       "e1:q9:find_node1:t";
       bencodeStr r.qfn_t;
-      "1:y1:qe"
+      "1:y1:q";
+      "e"
     ]
 ;;
     
@@ -172,9 +181,13 @@ let bencoded_to_PingAnswer b=
 ;;
 
 
-let decoupe_nodes s = 
-  (String.sub s 0 20)::(String.sub s 20 20)::(String.sub s 40 20)::(String.sub s 60 20)::(String.sub s 80 20)::(String.sub s 100 20)::(String.sub s 120 20)::(String.sub s 140 20)::[]
+let rec decoupe_nodes = function
+  | "" -> []
+  | s ->
+      let n = String.length s in
+      (String.sub s 0 26)::(decoupe_nodes (String.sub s 21 (n-26)))
 ;;
+
       
       
 let bencoded_to_Find_NodesAnswer b= 
@@ -266,35 +279,35 @@ let envoie_requetes requestsToSend =
   |(QFindNode x, serv_addr)::requestsToSend' -> ignore(envoie_requeteFind_nodes (QFindNode x) serv_addr) (*supprimer les ignore*)
 
 
-let rec trouve_noeud requestsToSend =
-(*on démarre cette fonction avec requestsToSend ayant un seul élément, un QFindNode vers un certain noeud en cherchant un certain targetNode.
-Cette fonction s'arrete lorsque toutes les requetes ont été effectuées.*) 
-    match requestsToSend with
-    |[] -> ()
-    |(QPing x, serv_addr)::requestsToSend' -> begin ignore(envoie_requetePing (QPing x) serv_addr); trouve_noeud requestsToSend' end
-    |(QFindNode x, serv_addr)::requestsToSend' -> 
-      begin
-	let answer = envoie_requeteFind_nodes (QFindNode x) serv_addr in
-	if ((*on nous renvoie l'addresse ip du targetNode*) false) 
-	then  (*alors on le ping*)
-	  begin 
-	    incr trans_num; 
-	    trouve_noeud ((QPing{qp_t = (int_to_trans_num trans_num); qp_id= !targetNode} , (*normalement nv_serv_addr*) serv_addr)::(requestsToSend'))
-	  end
-	else if ((*on nous renvoie l'addresse ip d'un noeud plus proche*) false)
-	then (*on lui demande où est targetNode*)
-	  begin
-	    incr trans_num; 
-	    trouve_noeud ((QFindNode {qfn_t = (int_to_trans_num trans_num) ; qfn_id=myID; qfn_target = !targetNode},  (*normalement nv_serv_addr*) serv_addr)::requestsToSend')
-	  end
-	else (*on nous renvoie les ids d'autres noeuds plus proches*)
-	  begin
-	    let nvellesreqs = List.map 
-	      (fun x ->  incr trans_num ; (QFindNode {qfn_t = (int_to_trans_num trans_num) ; qfn_id=myID; qfn_target =x}, serv_addr)) answer.afn_nodes in
-	     trouve_noeud (nvellesreqs@requestsToSend') (*est-ce qu'il vaut mieux faire d'abord les novuelles requetes ou celles d'avant?*)
-	  end
-      end
-;;
+(* let rec trouve_noeud requestsToSend = *)
+(* (\*on démarre cette fonction avec requestsToSend ayant un seul élément, un QFindNode vers un certain noeud en cherchant un certain targetNode. *)
+(* Cette fonction s'arrete lorsque toutes les requetes ont été effectuées.*\)  *)
+(*     match requestsToSend with *)
+(*     |[] -> () *)
+(*     |(QPing x, serv_addr)::requestsToSend' -> begin ignore(envoie_requetePing (QPing x) serv_addr); trouve_noeud requestsToSend' end *)
+(*     |(QFindNode x, serv_addr)::requestsToSend' ->  *)
+(*       begin *)
+(* 	let answer = envoie_requeteFind_nodes (QFindNode x) serv_addr in *)
+(* 	if ((\*on nous renvoie l'addresse ip du targetNode*\) false)  *)
+(* 	then  (\*alors on le ping*\) *)
+(* 	  begin  *)
+(* 	    incr trans_num;  *)
+(* 	    trouve_noeud ((QPing{qp_t = (int_to_trans_num trans_num); qp_id= !targetNode} , (\*normalement nv_serv_addr*\) serv_addr)::(requestsToSend')) *)
+(* 	  end *)
+(* 	else if ((\*on nous renvoie l'addresse ip d'un noeud plus proche*\) false) *)
+(* 	then (\*on lui demande où est targetNode*\) *)
+(* 	  begin *)
+(* 	    incr trans_num;  *)
+(* 	    trouve_noeud ((QFindNode {qfn_t = (int_to_trans_num trans_num) ; qfn_id=myID; qfn_target = !targetNode},  (\*normalement nv_serv_addr*\) serv_addr)::requestsToSend') *)
+(* 	  end *)
+(* 	else (\*on nous renvoie les ids d'autres noeuds plus proches*\) *)
+(* 	  begin *)
+(* 	    let nvellesreqs = List.map  *)
+(* 	      (fun x ->  incr trans_num ; (QFindNode {qfn_t = (int_to_trans_num trans_num) ; qfn_id=myID; qfn_target =x}, serv_addr)) answer.afn_nodes in *)
+(* 	     trouve_noeud (nvellesreqs@requestsToSend') (\*est-ce qu'il vaut mieux faire d'abord les novuelles requetes ou celles d'avant?*\) *)
+(* 	  end *)
+(*       end *)
+(* ;; *)
 
 (* let receiver sck queue stats () = *)
 (*   let n = ref 0 in (\*number of packet handled, for debugging purposes*\) *)
@@ -318,22 +331,32 @@ let random_id () =
   buf
 ;;
 
-let testlol requeteFind_Nodes serv_addr=
+let reverseString s =
+  let n = String.length s in
+  let s' = String.make n 'c' in
+  for i = 0 to (n-1) do
+    s'.[i] <- s.[n-i-1];
+  done;
+  s'
+;;
+
+let testlol id want =
   let s = socket PF_INET SOCK_DGRAM 0 in
-  let bencodedFind_nodesQuery=bencodeQuery ( requeteFind_Nodes ) in    (*QFindNode {qfn_t = "aa"; qfn_id="abcdefghij0123456789"; qfn_target = target})) in*)
-  ignore ( sendto s bencodedFind_nodesQuery 0 (String.length bencodedFind_nodesQuery) [] serv_addr); 
+  let bencodedFind_nodesQuery= bencodeQuery (QFindNode {qfn_t = "aa"; qfn_id="abcdefghij0123456789"; qfn_target = id; qfn_want = want} ) in
+  ignore (sendto s bencodedFind_nodesQuery 0 (String.length bencodedFind_nodesQuery) [] addrBootstrap); 
   let buffer_reponse = String.create 1500 in
   ignore ( recvfrom s buffer_reponse 0 1500 []);
   close s;
   parser buffer_reponse;
 ;;
 
+
 let main =
   let i  = ref 0 in
   let ens = ref (IDSet.empty) in
   while (true) do
     let id_to_check = random_id () in 
-    let answer = envoie_requeteFind_nodes (QFindNode {qfn_t = "aa"; qfn_id="12345678901234567890"; qfn_target = id_to_check}) addrBootstrap in
+    let answer = envoie_requeteFind_nodes (QFindNode {qfn_t = "aa"; qfn_want = 1; qfn_id="12345678901234567890"; qfn_target = id_to_check}) addrBootstrap in
     if (List.length answer.afn_nodes >= 2) 
     then ens := (List.fold_left (fun set x->IDSet.add x set) !ens answer.afn_nodes);
     Printf.printf "Taille de la table découverte: %i a la %dieme requete" (IDSet.cardinal !ens) !i;
