6dc7e1723240f571239da0d2d58bdb0ad623226a
Gwendoline <chouasne@clipper.ens.fr>
2015-11-21 17:33:35 +0100

    supprimé les @@

--- tmp.ml
+++ tmp.ml
@@ -197,9 +197,9 @@ let bencoded_to_Find_NodesAnswer b=
 let envoie_requetePing requetePing serv_addr =
   let s = socket PF_INET SOCK_DGRAM 0 in
   let bencodedPingQuery=bencodeQuery ( requetePing ) in                     (*QPing {qp_t = "aa"; qp_id="abcdefghij0123456789"})) in*)
-  ignore @@ sendto s bencodedPingQuery 0 (String.length bencodedPingQuery) [] serv_addr; 
+  ignore ( sendto s bencodedPingQuery 0 (String.length bencodedPingQuery) [] serv_addr); 
   let buffer_reponse = String.create 1500 in
-  ignore @@ recvfrom s buffer_reponse 0 1500 [];
+  ignore ( recvfrom s buffer_reponse 0 1500 []);
   close s;
   bencoded_to_id (parser buffer_reponse)
 ;;
