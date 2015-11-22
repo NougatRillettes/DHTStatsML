37ee0929680cb50d8656eed5fb9c301d9b7503cb
Arthur Carcano <arthur.carcano@orange.fr>
2015-11-22 07:01:03 +0100

    Corrected fifo

--- FIFO.ml
+++ FIFO.ml
@@ -13,21 +13,21 @@ let pop (n,l,_) =
   match (!n,!l) with
     | (0,[]) -> raise EmptyFifo;
     | (_,x::xs) -> decr n; l := xs; x
-    | _ -> failwith "Fifo STructure violated !"
+    | _ -> failwith "Fifo Structure violated !"
 ;;
 
 let empty (n,_,_) = 
   !n == 0
 ;;
 
-let size (_,_,m) = m;;
+let size (n,_,_) = !n;;
 
 let push x (n,l,max) =
   if !n == max then
     begin
       (* l := List.rev (List.tl (List.rev !l)); *)
       l := [];
-      decr n;
+      n := 0;
     end;
   incr n;
   l := (x::!l)
