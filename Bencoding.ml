46b121df05041d5a820e6a3a6736e2014441f302
Arthur Carcano <arthur.carcano@orange.fr>
2015-11-22 05:54:24 +0100

    Changed to bad answer in pasrer (2)

--- Bencoding.ml
+++ Bencoding.ml
@@ -126,7 +126,7 @@ let parser s =
           BDic [];
       | x when '0' <= x && x <= '9' ->
           BString (parse_string ());
-      | _ -> Bad_Answer "Parse error :("
+      | _ -> raise (Bad_Answer "Parse error :(")
   in
   aux ()
 ;;
